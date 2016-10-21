ae38fac966c91a05d9c0a572d6f2c9abb72fdb09
Oleg Lutsenko <oleg.lutsenko@gmail.com>
2016-10-21 23:15:16 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Curl",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/CCurl.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/IBM-Swift/CCurl.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 12),
     ]
 )
